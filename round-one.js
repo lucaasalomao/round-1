ee65078a53d4b063697900209ef4375c0721255a
Lucas Salomao <lucaas.afonso@hotmail.com>
2021-12-18 11:58:40 -0300

    Adicionando listener das setas e mudança das variáveis de posição

--- round-one.js
+++ round-one.js
@@ -1,7 +1,19 @@
-window.onload = () => {
+
+
+class Player {
+    constructor() {
+        this.positionX = 10;
+        this.positionY = 175;
+    }
+    move() {
+        
+    }
+}
+
+function startGame(){
     const canvas = document.getElementById('game-space');
     const context = canvas.getContext('2d');
-
+    
     const ground = new Image();
     ground.src = "C:/Users/27095/Desktop/git-practices/round-1/images/ground.jpg";
     const player = new Image();
@@ -9,16 +21,36 @@ window.onload = () => {
     const doll = new Image();
     doll.src = "C:/Users/27095/Desktop/git-practices/round-1/images/doll.png";
 
+    gameStart = false
+    
     document.getElementById('start-button').onclick = () => {
-        console.log("Apertei")
         context.drawImage(ground,0,0)
         context.drawImage(player,10,175,20,30)
         context.drawImage(doll,520,140,70,100)
+        gameStart = true
     };
-    
-    function movePlayer() {
-        
+
+    let canvasVariables = [canvas, context,gameStart]
+
+    return canvasVariables
+}
+
+function restartGame(canvasVariables){
+    console.log(canvasVariables[2])
+    if(canvasVariables[2]){
+        document.getElementById('restart-button').onclick = () => {
+            canvasVariables[1].clearRect(0, 0, canvasVariables[0].width, canvasVariables[0].height)
+            console.log("Estamos dando restart")
+            startGame()
+        };
     }
+}
+
+window.onload = () => {
+    let canvasVariables = startGame()
+    setInterval(function() {
+        restartGame(canvasVariables)}
+        , 5000);
 }
 
 
